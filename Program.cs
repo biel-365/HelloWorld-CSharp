984d1e3a286f0b4f806a7f3aea23355b9e20ff76
biel-365 <aroiogabriel@gmail.com>
2025-07-31 10:32:50 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var numero1 = 2;
+        var numero2 = 3;
+
+        var soma = numero1 + numero2;
+
+        Console.WriteLine("Bom");
+        Console.WriteLine("Dia");
+        Console.WriteLine("A soma é igual a:" + soma);
+    }
+}
